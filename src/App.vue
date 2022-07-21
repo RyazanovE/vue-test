65e3ad99e74d7533282d12867bccafd91007a1fe
RyazanovE <sidewipe@yandex.ru>
2022-07-21 22:39:35 +0300

    fonts_add

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <div class="container">
       <card-create-form @create="createItem" :cards="cards" />
       <card-list v-if="cards.length" @remove="removeItem" :cards="cards" />
-      <img class="loader" src="public\images\loader.gif" v-else-if="isLoading" />
+      <img
+        class="loader"
+        src="public\images\loader.gif"
+        v-else-if="isLoading"
+      />
     </div>
   </div>
 </template>
@@ -126,151 +130,15 @@ export default {
 </script>
 
 <style lang="scss">
-@media screen and (max-width: 1200px) {
-  .wrapper {
-    padding: 16px;
-  }
-  .container {
-    flex-direction: column;
-  }
+@import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap");
+@import "./reset.scss";
+
+* {
+  font-family: "Source Sans Pro", sans-serif !important;
 }
 
 .loader {
   transform: scale(0.2);
-}
-
-html,
-body,
-description,
-div,
-span,
-applet,
-object,
-iframe,
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-p,
-blockquote,
-pre,
-a,
-abbr,
-acronym,
-address,
-big,
-cite,
-code,
-del,
-dfn,
-em,
-img,
-ins,
-kbd,
-q,
-s,
-samp,
-small,
-strike,
-strong,
-sub,
-sup,
-tt,
-var,
-b,
-u,
-i,
-center,
-dl,
-dt,
-dd,
-ol,
-ul,
-li,
-fieldset,
-form,
-label,
-legend,
-table,
-caption,
-tdescription,
-tfoot,
-thead,
-tr,
-th,
-td,
-article,
-aside,
-canvas,
-details,
-embed,
-figure,
-figcaption,
-footer,
-header,
-hgroup,
-menu,
-nav,
-output,
-ruby,
-section,
-summary,
-time,
-mark,
-audio,
-video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article,
-aside,
-details,
-figcaption,
-figure,
-footer,
-header,
-hgroup,
-menu,
-nav,
-section {
-  display: block;
-}
-description {
-  line-height: 1;
-}
-input,
-textarea,
-button {
-  font-family: inherit;
-  font-size: inherit;
-  outline: none;
-  border: none;
-}
-ol,
-ul {
-  list-style: none;
-}
-blockquote,
-q {
-  quotes: none;
-}
-blockquote:before,
-blockquote:after,
-q:before,
-q:after {
-  content: "";
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
 }
 
 .wrapper {
@@ -284,6 +152,15 @@ table {
 
   & > * {
     flex: 1;
+  }
+}
+
+@media screen and (max-width: 1200px) {
+  .wrapper {
+    padding: 16px;
+  }
+  .container {
+    flex-direction: column;
   }
 }
 </style>
